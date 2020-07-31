0300f93e6eb63240004c0ae6feba07092eff8c19
Fernando <Fernied80@gmail.com>
2020-07-30 22:24:36 -0600

    first commit to beer

---
+++ Beer.py
@@ -0,0 +1,82 @@
+import speech_recognition as sr
+import time
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+
+if __name__ == "__main__":
+    # set the list of words, maxnumber of guesses, and prompt limit
+    WORDS = ["beer", "me"]
+    # NUM_GUESSES = 3
+    # PROMPT_LIMIT = 5
+
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # get a random word from the list
+    # word = random.choice(WORDS)
+
+    # format the instructions string
+    instructions = (f'Tell me what you want is it a {WORDS}')
+    # show instructions and wait 3 seconds before starting the game
+    print(instructions)
+    time.sleep(3)
+    print('speak now or forever hold your peace')
+    want = recognize_speech_from_mic(recognizer, microphone)
+
+    print(want)
+
+    for key in want.keys():
+        if key == 'transcription':
+            #@todo  have to find a way to read the transcription to be able to compare to 'beer'
+
+           #@todo show image of beer me brother
+
